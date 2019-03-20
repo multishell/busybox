39f00b48cd5872c3067a41aac6cbf6caa939efe7
Tyler Akins <fidian@rumkin.com>
2019-03-20 10:50:05 -0500

    Removing bdflush since it is now automatic

--- init.c
+++ init.c
@@ -103,7 +103,7 @@ struct serial_struct {
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #if __GNU_LIBRARY__ > 5
-	#include <sys/kdaemon.h>
+	//#include <sys/kdaemon.h>
 #else
 	extern int bdflush (int func, long int data);
 #endif
@@ -736,7 +736,7 @@ static void shutdown_system(void)
 	sync();
 	if (kernelVersion > 0 && kernelVersion <= KERNEL_VERSION(2,2,11)) {
 		/* bdflush, kupdate not needed for kernels >2.2.11 */
-		bdflush(1, 0);
+		//bdflush(1, 0);
 		sync();
 	}
 }
