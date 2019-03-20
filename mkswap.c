0346f1dc15acc952f996ec7323ad5bca1334d58a
Tyler Akins <fidian@rumkin.com>
2019-03-20 10:53:08 -0500

    Removed unnecessary include asm/page.h

--- mkswap.c
+++ mkswap.c
@@ -44,7 +44,7 @@
 #include <sys/ioctl.h>			/* for _IO */
 #include <sys/utsname.h>
 #include <sys/stat.h>
-#include <asm/page.h>			/* for PAGE_SIZE and PAGE_SHIFT */
+//#include <asm/page.h>			/* for PAGE_SIZE and PAGE_SHIFT */
 				/* we also get PAGE_SIZE via getpagesize() */
 
 
