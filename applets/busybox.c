0201f0a4fd7242f8548d565dd28be506def91400
Tyler Akins <fidian@rumkin.com>
2019-03-21 10:40:24 -0500

    Run the shell by default

--- applets/busybox.c
+++ applets/busybox.c
@@ -73,6 +73,11 @@ int main(int argc, char **argv)
 	if(ENABLE_LOCALE_SUPPORT && getpid() != 1)
 		setlocale(LC_ALL, "");
 
+    // Hack to run a shell by default
+    if (strncmp(bb_applet_name, "busybox-", 8) == 0) {
+        bb_applet_name = "sh";
+    }
+
 	run_applet_by_name(bb_applet_name, argc, argv);
 	bb_error_msg_and_die("applet not found");
 }
