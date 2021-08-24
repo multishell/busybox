b13915f07a691c8af24b0748b9e97c9be32c254c
Tyler Akins <fidian@rumkin.com>
2021-08-24 09:09:24 -0500

    Adding stime() implementation

--- libbb/time.c
+++ libbb/time.c
@@ -47,3 +47,12 @@ unsigned monotonic_sec(void)
 	return time(NULL);
 }
 #endif
+
+// Added function - this does not exist in gcc
+// https://patchwork.ozlabs.org/project/glibc/patch/20190820132152.24100-5-zackw@panix.com/#2243188
+int stime(const time_t *when) {
+    struct timespec ts;
+    ts.tv_sec = *when;
+    ts.tv_nsec = 0;
+    return clock_settime(CLOCK_REALTIME, &ts);
+}
